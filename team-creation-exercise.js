8c67408194e9d75404b09e8f75bd08819a24685c
adiya <adiya.mohr@signavio.com>
2018-02-05 23:09:35 +0100

    add functionality to add games

--- team-creation-exercise.js
+++ team-creation-exercise.js
@@ -1,11 +1,41 @@
 const team = {
-  _players: [],
-  _games: [],
-  addPlayersToTeam(firstName, lastName, age) {
+  _players: [
+    { firstName: "Jose", lastName: "Gonzalez", age: 23 },
+    { firstName: "Pablo", lastName: "Sanchez", age: 19 },
+    { firstName: "Cat", lastName: "Catchins", age: 21 }
+  ],
+  _games: [
+    { opponent: "Broncos", teamPoints: 42, opponentPoints: 27 },
+    { opponent: "Broncos", teamPoints: 42, opponentPoints: 27 },
+    { opponent: "Broncos", teamPoints: 42, opponentPoints: 27 }
+  ],
+  addPlayer(firstName, lastName, age) {
     const player = {
       firstName: firstName,
       lastName: lastName,
       age: age
     };
+    this._players.push(player);
+  },
+  addGame(opponent, teamPoints, opponentPoints) {
+    const game = {
+      opponent: opponent,
+      teamPoints: teamPoints,
+      opponentPoints: opponentPoints
+    };
+    this._games.push(game);
+  },
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._games;
   }
 };
+
+team.addPlayer("Steph", "Curry", 28);
+team.addPlayer("Lisa", "Leslie", 44);
+team.addPlayer("Bugs", "Bunny", 76);
+team.addGame("Blahblahs", 34, 56);
+console.log(team.players);
+console.log(team.games);
