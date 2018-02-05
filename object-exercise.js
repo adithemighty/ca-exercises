97fcdc364191e00937c5b996d4319420e26a1449
adiya <adiya.mohr@signavio.com>
2018-02-05 16:09:33 +0100

    add object exercise with menu creation example

---
+++ object-exercise.js
@@ -0,0 +1,52 @@
+const menu = {
+    _courses: {
+        _appetizers: [],
+        get appetizers() {
+            return this._appetizers;
+        },
+        set appetizers(appetizerIn) {
+            this._appetizers.push(appetizerIn)
+        },
+        _mains: [],
+        get mains() {
+            return this._mains;
+        },
+        set mains(mainsIn) {
+            this._mains.push(mainsIn)
+        },
+        _desserts: [],
+        get desserts() {
+            return this._desserts;
+        },
+        set desserts(dessertsIn) {
+            this._desserts.push(dessertsIn)
+        }
+    },
+    get courses() {
+        return {
+            appetizers: this._courses.appetizers,
+            mains: this._courses.mains,
+            desserts: this._courses.desserts
+        }
+    },
+    addDishToCourse(courseName, dishName, dishPrice) {
+        const dish = {
+            name: dishName,
+            price: dishPrice
+        }
+        this._courses[courseName].push(dish);
+    },
+    getRandomDishFromCourse(courseName) {
+        const dish = this.courses[courseName]
+        const randomNumber = Math.floor((Math.random() * dish.length))
+        return dish[randomNumber]
+    },
+    generateRandomMeal(){
+        const appetizer = this.getRandomDishFromCourse("appetizers")
+        const main = this.getRandomDishFromCourse("mains")
+        const dessert = this.getRandomDishFromCourse("desserts")
+        const meal = [appetizer.name, main.name, dessert.name]
+        const price = [appetizer.price, main.price, dessert.price]
+        return `you get ${meal.join(", ")} for ${price.reduce(reducer = (accumulator, currentValue) => accumulator + currentValue)} pesos.`
+    }
+};
