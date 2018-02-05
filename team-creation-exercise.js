e01796a8654c7015d99fc59508d12117d6fe2e11
adiya <adiya.mohr@signavio.com>
2018-02-05 17:16:19 +0100

    add file for team creation exercise with first team object

---
+++ team-creation-exercise.js
@@ -0,0 +1,11 @@
+const team = {
+  _players: [],
+  _games: [],
+  addPlayersToTeam(firstName, lastName, age) {
+    const player = {
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    };
+  }
+};
